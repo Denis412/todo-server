47f39799c4f2fa218aab26f2fa06f934645a169c
Denis <47417617+Denis412@users.noreply.github.com>
2024-04-19 20:51:10 +0300

    Добавлена валидация на поля model_id и owner_id в CreatePermissionInput

--- src/app.module.ts
+++ src/app.module.ts
@@ -31,7 +31,6 @@ import { AppAuthGuard } from './auth/guards/auth.guard';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [],
       synchronize: true,
       autoLoadEntities: true,
     }),
--- src/permission/dto/create-permission.input.ts
+++ src/permission/dto/create-permission.input.ts
@@ -2,12 +2,17 @@ import { InputType, Int, Field } from '@nestjs/graphql';
 import { PermissionConfigInput } from './permission-config.input';
 import { OwnerType } from '../types/owner-type.enum';
 import { ModelType } from '../types/model-type.enum';
+import { IsString, Matches } from 'class-validator';
 
 @InputType()
 export class CreatePermissionInput {
+  @IsString()
+  @Matches(/^[0-9]+$/, { message: 'String must consist only of digits' })
   @Field()
   owner_id: string;
 
+  @IsString()
+  @Matches(/^[0-9]+$/, { message: 'String must consist only of digits' })
   @Field()
   model_id: string;
 
